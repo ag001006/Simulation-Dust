7f6b19becab4db074db929696ff88c2fffa1421b
Lu-Yi <ag001006@gmail.com>
2019-08-28 09:11:57 +0800

    建立資料結構雛形

--- Assets/Main Assets/Scripts/Data/CharacterData.cs
+++ Assets/Main Assets/Scripts/Data/CharacterData.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterData : MonoBehaviour
+public class CharacterData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string CharacterName = string.Empty;
+    public ClassType classType = 0;
+    public BasicAbility basicAbility = new BasicAbility();
+    public Texture icon;
 }
+
+/// <summary> 職業 </summary>
+public enum ClassType
+{
+    /// <summary> 攻擊型 </summary>
+    Warrior,
+    /// <summary> 防禦型 </summary>
+    Defender,
+    /// <summary> 魔法型 </summary>
+    Magician,
+    /// <summary> 支援型 </summary>
+    Supporter
+}
+
+public struct BasicAbility
+{
+    public float Damage = 0;
+    public float Health = 0;
+    public float Reduce = 0;
+    public float Critical = 0;
+    public float CriticalEffect = 0;
+    public float Dodge = 0;
+}
+
